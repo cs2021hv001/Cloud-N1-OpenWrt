89adcb209e24f4233e566c7cb679193c420a2f95
siren202101 <80493699+siren202101@users.noreply.github.com>
2026-05-01 09:17:44 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ sed -i 's|^#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05|
 #sed -i '$a src-git kiddin9_openwrt_packages https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 #sed -i '$a src-git kenzok8_openwrt_packages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 #sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/small-package.git' feeds.conf.default
-sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/jell.git' feeds.conf.default
+sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/small-package.git' feeds.conf.default
 #sed -i '1i src-git OpenClash https://github.com/vernesong/OpenClash.git' feeds.conf.default
 sed -i '1i src-git openwrt_passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall.git' feeds.conf.default
 sed -i '1i src-git openwrt_passwall2_packages https://github.com/Openwrt-Passwall/openwrt-passwall2.git' feeds.conf.default
